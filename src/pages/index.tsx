93eca641637aca6e27e43b90a99b6a6992a2abda
Kenny Chu <kennychu0510@gmail.com>
2023-09-19 19:58:05 +0800

    add max scale to 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,7 +84,7 @@ export default function Home() {
       <Head>
         <title>League Schedule Generator</title>
         <meta name="description" content="League Schedule Generator" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
